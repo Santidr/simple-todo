13996ebe54a1cfcea489260864852891adadcc36
Santiago Díaz <kerbindr@gmail.com>
2016-04-01 22:02:43 -0400

    Nuevos checkboxes personalizados

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,27 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     minifyCss = require('gulp-minify-css'),
-    webserver = require('gulp-webserver');
+    connect = require('gulp-connect');
 
 gulp.task('sass', function() {
     return gulp.src('./sass/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('html', function() {
+    gulp.src('*.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+    gulp.src('./js/*.js')
+        .pipe(connect.reload());
 });
 
 gulp.task('minjs', function() {
-    gulp.src('./js/*.js')
+    gulp.src('./**/*.js')
     .pipe(uglify())
     .pipe(rename('app.min.js'))
     .pipe(gulp.dest('./dist/js/'));
@@ -27,17 +38,17 @@ gulp.task('mincss', function() {
 
 gulp.task('dist', ['minjs', 'mincss']);
 
-gulp.task('webserver', ['watch'], function() {
-  gulp.src('/home/kerbin/desktop-todo')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+gulp.task('connect', function() {
+    connect.server({
+        root: '/home/kerbin/simple-todo',
+        livereload: true
+    });
 });
 
 gulp.task('watch', function() {
     gulp.watch('./sass/*.scss', ['sass']);
+    gulp.watch('*.html', ['html']);
+    gulp.watch('./**/*.js', ['js']);
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('default', ['connect', 'watch']);
